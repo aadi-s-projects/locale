e18c9fcb177c53833211be571ccb37fe0f3b4dfc
Aadi Gala <aadi.gala@icloud.com>
2024-06-25 18:56:36 -0400

    Initial Commit

---
+++ locale/localeApp.swift
@@ -0,0 +1,17 @@
+//
+//  localeApp.swift
+//  locale
+//
+//  Created by Sachin Gala on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct localeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
